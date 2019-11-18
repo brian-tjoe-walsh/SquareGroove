9b94de4dd61fde5e9791e64163722f511dd6d26f
brian tjoe walsh <bbriannwalshh@gmail.com>
2019-11-18 11:29:46 -0800

    commit partial user auth

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-
-app.get("/", (req, res) => res.send("Welcome to VibeLoop"));
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const grids = require("./routes/api/grids");
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+
+app.use(bodyParser.urlencoded({ 
+  extended: false 
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: 'vibe',
+    email: 'vibe@vibe.vibe',
+    password: 'issaVibe'
+  });
+
+  user.save();
+  res.send("Hello World!");
+});
+
+app.use("/api/users", users);
+app.use("/api/grids", grids);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
