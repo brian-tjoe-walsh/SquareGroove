9e81a2731cae37ef311f7ce6e9285dda3a608002
brian tjoe walsh <bbriannwalshh@gmail.com>
2019-11-25 09:29:20 -0800

    change from build to public

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(bodyParser.json());
 
     
       if (process.env.NODE_ENV === 'production') {
-        app.use(express.static('frontend/build'));
+        app.use(express.static('frontend/public'));
         app.get('/', (req, res) => {
           res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
         });
